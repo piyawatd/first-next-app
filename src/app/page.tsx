a744cd11b3a740dc2d2dabd4d709463b4616f334
piyawat <arunwatd@gmail.com>
2025-05-02 09:06:43 +0700

    update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="tracking-[-.01em]">
+            Add New Line
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
